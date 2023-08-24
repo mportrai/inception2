fad6dfe79969e67239cc82aa1b6d48b614f1c551
Maxime PORTRAIT <mportrai@paul-f5Ar3s8.clusters.42paris.fr>
2023-08-24 09:17:19 +0200

    reviens a une version plus ancienne

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,38 +3,43 @@
 
 sleep 15
 echo " Mes variables sont $SQL_DATABASE $SQL_USER $SQL_PASSWORD $DOMAIN_NAME $WEBSITE_NAME $WP_ADMIN $WP_ADMIN_PASSWORD $WP_ADMIN_EMAIL $WP_USER $WP_USER_EMAIL $WP_USER_PASSWORD"
-
+# while ! mysqladmin ping -h mariadb -u"$SQL_USER" -p"$SQL_PASSWORD" --silent;
+# do
+#     echo "Attente du service MySQL..."
+#     sleep 2
+# done
 echo "Lancement script wordpress"
-
 	echo "condition"
-	wp core download --allow-root --locale=fr_FR --path='/var/www/html/'
+	wp core download --allow-root --locale=fr_FR 
+	# --path='/var/www/html/wordpress'
 	sleep 10
 	wp config create --allow-root \
 				--dbname=$SQL_DATABASE \
 				--dbuser=$SQL_USER \
 				--dbpass=$SQL_PASSWORD \
 				--dbhost=mariadb:3306 \
-				--path='/var/www/html/'
+				
+				# --path='/var/www/html/wordpress'
 	echo "sleep"	
 	sleep 2
-
+	chmod 700 wp-config.php
 	wp core install --url=$DOMAIN_NAME --title=$WEBSITE_NAME \
 				--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root \
-				--path='/var/www/html/'
-
+				--admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
+				# --path='/var/www/html/wordpress'
 	sleep 2
 	wp user create --allow-root --role=author $WP_USER $WP_USER_EMAIL \
-				--user_pass=$WP_USER_PASSWORD \
-				--path='/var/www/html/'
-
-
+				--user_pass=$WP_USER_PASSWORD 
+				# --path='/var/www/html/wordpress'
 echo "plus condition"
+# if	[ ! -d /run/php ]; then
+# 	mkdir /run/php
+# fi
 
-mkdir -p /run/php/7.4/fpm
-chown -R www-data:www-data /run/php/7.4/fpm
-chmod -R 755 /var/www/html/
-chown -R www-data:www-data /var/www/html/
+RUN mkdir -p /run/php/7.4/fpm
+RUN chown -R www-data:www-data /run/php/7.4/fpm
+RUN chown -R www-data:www-data /var//www/*
+RUN chmod -R 777 /var/www/*
 
 /usr/sbin/php-fpm7.4 -F
 
