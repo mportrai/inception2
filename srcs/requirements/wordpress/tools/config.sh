ce56a79dcbca0e7ba2578d5247885ca829e58ada
Maxime PORTRAIT <mportrai@paul-f5Ar3s8.clusters.42paris.fr>
2023-08-24 12:15:07 +0200

    modif, retirer des comments etc

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-
 sleep 15
+
 echo "Lancement script wordpress"
-	echo "condition"
 	wp core download --allow-root --locale=fr_FR --path='/var/www/html'
 	sleep 10
 	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER \
 				--dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 \
 				--path='/var/www/html'
 	sleep 2
-	# chmod 755 wp-config.php
 	wp core install --url=$DOMAIN_NAME --title=$WEBSITE_NAME \
 				--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD \
 				--admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root \
@@ -18,13 +16,9 @@ echo "Lancement script wordpress"
 	sleep 2
 	wp user create --allow-root --role=author $WP_USER $WP_USER_EMAIL \
 				--user_pass=$WP_USER_PASSWORD --path='/var/www/html'
-echo "plus condition"
-
 mkdir -p /run/php/7.4/fpm
 chown -R www-data:www-data /run/php/7.4/fpm
 chown -R www-data:www-data /var//www/*
 chmod -R 777 /var/www/*
 
 /usr/sbin/php-fpm7.4 -F
-
-echo "Finito"
